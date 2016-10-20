a310019db934738f84dcd68fdca060eebc8736d8
Renee Vrantsidis <renee.vrantsidis@rangle.io>
2016-10-20 11:31:44 -0400

    Created exercise folder for unit testing and started configuration

---
+++ exercises/unit-tests/karma.config.js
@@ -0,0 +1,74 @@
+module.exports = (config) => {
+  const coverage = config.singleRun ? ['coverage'] : [];
+
+  config.set({
+    frameworks: [
+      'jasmine',
+    ],
+
+    plugins: [
+      'karma-jasmine',
+      'karma-sourcemap-writer',
+      'karma-sourcemap-loader',
+      'karma-webpack',
+      'karma-coverage',
+      'karma-remap-istanbul',
+      'karma-spec-reporter',
+      'karma-chrome-launcher',
+    ],
+
+    files: [
+      './src/tests.entry.ts',
+      {
+        pattern: '**/*.map',
+        served: true,
+        included: false,
+        watched: true,
+      },
+    ],
+
+    preprocessors: {
+      './src/tests.entry.ts': [
+        'webpack',
+        'sourcemap',
+      ],
+      './src/**/!(*.test|*.spec|tests.*).(ts|js)': [
+        'sourcemap',
+      ],
+    },
+
+    webpack: {...},
+
+    webpackServer: {
+      noInfo: true, // prevent console spamming when running in Karma!
+    },
+
+    reporters: ['spec']
+      .concat(coverage)
+      .concat(coverage.length > 0 ? ['karma-remap-istanbul'] : []),
+
+    remapIstanbulReporter: {
+      src: 'coverage/chrome/coverage-final.json',
+      reports: {
+        html: 'coverage',
+      },
+    },
+
+    coverageReporter: {
+      reporters: [
+        { type: 'json' },
+      ],
+      dir: './coverage/',
+      subdir: (browser) => {
+        return browser.toLowerCase().split(/[ /-]/)[0]; // returns 'chrome'
+      },
+    },
+
+    port: 9999,
+    browsers: ['Chrome'], // Alternatively: 'PhantomJS'
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    captureTimeout: 6000,
+  });
+};
