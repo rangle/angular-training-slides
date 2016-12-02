a9888c5f96bb222b16a92c03294979170e87ef65
Mark Pevec <mark.pevec@rangle.io>
2016-12-02 17:18:24 -0500

    PR30 issue fixes

--- exercises/pipes/exercise/src/app.module.ts
+++ exercises/pipes/exercise/src/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule }  from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FilesizePipe } from './format-file-size.pipe'
-import { App } from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [App, FilesizePipe],
-  bootstrap: [App]
+  declarations: [AppComponent, FilesizePipe],
+  bootstrap: [AppComponent]
 })
 export class MyAppModule {
 
