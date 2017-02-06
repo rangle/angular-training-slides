cd37d2a1c082d5b9ec0a04c7b66010c873d882d4
Russell Hoy <russellhoy@Russells-MacBook-Pro-2.local>
2017-02-06 15:25:24 -0500

    add child route with user details to solution

--- solutions/routing/src/app/app.routes.ts
+++ solutions/routing/src/app/app.routes.ts
@@ -2,13 +2,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsersHomeComponent, UsersContainerComponent } from '../containers/users.component';
 import HomeComponent from '../components/home.component';
 import UserDetailComponent from '../components/user-detail.component';
+import UserListComponent from '../components/user-list.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'users', component: UsersContainerComponent},
-  { path: 'users/:id', component: UserDetailComponent},
+  { path: 'users', component: UsersContainerComponent,
+    children: [
+  	{ path: '', component: UsersHomeComponent },
+  	{ path: ':id', component: UserDetailComponent },
+    ]
+  }
 ];
 
 export const APP_ROUTER_PROVIDERS = [];
