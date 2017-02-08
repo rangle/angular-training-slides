bfd92aad732bf19269aa288b5802a0529bc786d4
shaun lloyd <shaunlloydmedia@gmail.com>
2017-02-08 17:07:52 -0500

    Create simple exercise for formBuilder

--- solutions/form-builder/src/app/app.component.ts
+++ solutions/form-builder/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  people = []
+
+  firstName = new FormControl('')
+  lastName = new FormControl('')
+  email = new FormControl('')
+
+  contactForm: FormGroup = this.builder.group({
+    firstName: this.firstName,
+    lastName: this.lastName,
+    email: this.email
+  });
+
+  constructor(private builder: FormBuilder) {}
+
+  addPerson() {
+    this.people.push(this.contactForm.value);
+    this.contactForm.reset();
+  }
 }
--- solutions/form-builder/src/app/app.module.ts
+++ solutions/form-builder/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
