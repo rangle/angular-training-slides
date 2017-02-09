625e3ba4df885822b8a0fc23fd421a1fc55dbc0e
shaun lloyd <shaunlloydmedia@gmail.com>
2017-02-09 16:53:49 -0500

    Remove unused FormsModule from app module

--- solutions/form-builder/src/app/app.module.ts
+++ solutions/form-builder/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -11,7 +11,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule,
     HttpModule
   ],
