df89b5f8c058b5fa3bd27236762f9a20d1ac0b5a
Philip Da Silva <philip0043@gmail.com>
2017-02-13 14:01:32 -0500

    Adds exercise for angular-cli commands (#123)

---
+++ solutions/angular-cli/src/app/hey/hey.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { HeyDirective } from './hey.directive';
+
+describe('HeyDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HeyDirective();
+    expect(directive).toBeTruthy();
+  });
+});
