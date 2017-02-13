359c3a62087050207995e61106ce92351f8bd8e2
Philip Da Silva <philip.dasilva@rangle.io>
2017-02-13 11:23:13 -0500

    Adds exercise for angular-cli commands

---
+++ solutions/angular-cli/src/app/capitalize/capitalize.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CapitalizePipe } from './capitalize.pipe';
+
+describe('CapitalizePipe', () => {
+  it('create an instance', () => {
+    const pipe = new CapitalizePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
