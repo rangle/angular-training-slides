37c58deff462e0b1072b639e612bd1417810e882
Philip Da Silva <philip.dasilva@rangle.io>
2017-04-13 17:12:32 -0700

    Alternatve Testing Exampel

--- avery-app/src/app/todo-list/todo-list.component.spec.ts
+++ avery-app/src/app/todo-list/todo-list.component.spec.ts
@@ -2,13 +2,25 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
 
+// const todoActions: any = {
+//   someMethod: () => {}
+// }
+
+// const mockStore: any = {
+//   someMethod: () => {}
+// }
+
+
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
+      declarations: [ TodoListComponent ],
+      // providers: [
+      //   { provide: RealService, useClass: MockService }
+      // ]
     })
     .compileComponents();
   }));
@@ -17,6 +29,11 @@ describe('TodoListComponent', () => {
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    // component = new TodoListComponent(
+    //   todoActions as TodoActions,
+    //   mockStore as Store<any>
+    // );
   });
 
   it('should create', () => {
